c7a6b097b240af5dfb464c7286b93cc54705f13e
Riku <riku@seppa.la>
2011-11-20 19:32:05 +0200

    Tried to update indentation in models.py

--- upfo/upmain/models.py
+++ upfo/upmain/models.py
@@ -4,11 +4,11 @@ from django.contrib.auth.models import User
 # Create your models here.
 class upUser(models.Model):
 	user = models.ForeignKey(User)
-    name = models.CharField(max_length=40)
-    email = models.EmailField()
-    # shot = models.ImageField(upload_to='userimage')
+	name = models.CharField(max_length=40)
+	email = models.EmailField()
+	# shot = models.ImageField(upload_to='userimage')
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
-    class Admin: pass
+	class Admin: pass
